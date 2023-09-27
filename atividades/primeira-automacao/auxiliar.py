3a88601da2e8ba8e329378641f9f4078c7375de9
Cássio Queiroz Pereira <145166500+cassioqueirozpereira@users.noreply.github.com>
2023-09-26 23:48:02 -0300

    automação em python

--- atividades/primeira-automacao/auxiliar.py
+++ atividades/primeira-automacao/auxiliar.py
@@ -2,4 +2,6 @@ import pyautogui
 import time
 
 time.sleep(5)
-print(pyautogui.position())
+print(pyautogui.position()) # irá mostrar a posição do mouse
+
+pyautogui.scroll(10000) # irá rolar a barra para cima conforme o valor positivo. Para baixo se negativo.
--- atividades/primeira-automacao/codigo.py
+++ atividades/primeira-automacao/codigo.py
@@ -1,10 +1,12 @@
 import pyautogui # executar as tarefas
 import time # controlar o tempo entre as tarefas
+import pandas # importar a base de dados
 
 # pyautogui.click -> clicar com o mouse
 # pyautogui.write -> escrever um texto
 # pyautogui.press -> apertar 1 tecla
 # pyautogui.hotkey -> atalho (combinação de teclas)
+
 pyautogui.PAUSE = 0.5
 
 pyautogui.press("win")
@@ -13,6 +15,7 @@ pyautogui.press("enter")
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.write(link)
 pyautogui.press("enter")
+time.sleep(3)
 pyautogui.click(x=722, y=449) 
 # clicks=3 -> ele vai dar tres cliques
 # button="right" -> ele vai clicar com o botão direito
@@ -23,7 +26,36 @@ pyautogui.press("tab")
 pyautogui.press("enter")
 
 #pythonimpressionador@gmail.com sua senha aqui 
-import pandas # importar a base de dados
  
-tabela = pandas.read_csv("produtos.csv")
+tabela = pandas.read_csv("atividades/produtos.csv")
 print(tabela)
+
+for linha in tabela.index:
+
+    pyautogui.click(x=785, y=310)
+
+    codigo = tabela.loc[linha, "codigo"]
+
+    pyautogui.write(codigo)
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "categoria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(tabela.loc[linha, "custo"]))
+    pyautogui.press("tab")
+
+    obs = tabela.loc[linha, "obs"]
+    if not pandas.isna(obs):
+        pyautogui.write(str(obs))
+        
+    pyautogui.write(obs)
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    pyautogui.scroll(10000)
