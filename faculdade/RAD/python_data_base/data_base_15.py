86190d826c3986ff7981ca3c89d2390915b6db1f
Cássio Queiroz Pereira <145166500+cassioqueirozpereira@users.noreply.github.com>
2024-10-23 23:00:24 -0300

    Upgrade no programa da faculdade

--- faculdade/RAD/python_data_base/data_base_15.py
+++ faculdade/RAD/python_data_base/data_base_15.py
@@ -1,10 +1,17 @@
 import sqlite3 as conector
 from data_base_8 import Pessoa
 
+# conexão com um detector de types
 conexao = conector.connect("./meu_banco.db", detect_types = conector.PARSE_DECLTYPES)
 cursor = conexao.cursor()
 
+# função para converter números bytes em booleano
 def converter_booleano(dado):
+    # verifica se o dado é do type bytes
+    if isinstance (dado, bytes):
+        # converte para string
+        dado = dado.decode()
+        return True if dado == '1' else False
     return True if dado == 1 else False
 
 conector.register_converter("BOOLEAN", converter_booleano)
