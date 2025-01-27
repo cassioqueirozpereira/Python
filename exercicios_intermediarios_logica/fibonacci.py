f2a33387a2c14fadcd16242fb14a7d2b1938c2d0
Cássio Queiroz Pereira <145166500+cassioqueirozpereira@users.noreply.github.com>
2025-01-27 20:03:09 -0300

    treinando programacao

--- exercicios_intermediarios_logica/fibonacci.py
+++ exercicios_intermediarios_logica/fibonacci.py
@@ -1,4 +1,16 @@
-fibonacci = [1, 1]
+num = -1
 
-def f_fibonacci(fibonacci):
-    
+while (num < 0):
+    num = int(input("\nDigite um número inteiro positivo: "))
+    if (num < 0):
+        print("Você digitou um número negativo!")
+
+def f_fibonacci(num):
+    fibonacci = [1, 1]
+    print(fibonacci, num)
+    for i in range(num):
+        fibonacci.append(1)
+    return print(fibonacci)
+
+f_fibonacci(num)
+
