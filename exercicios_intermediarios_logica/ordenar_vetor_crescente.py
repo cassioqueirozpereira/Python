98fe377fbb4ea4453d2f32940d536d5f3abd49e8
Cássio Queiroz Pereira <145166500+cassioqueirozpereira@users.noreply.github.com>
2025-02-04 21:21:15 -0300

    treinando lógica de programacao

--- exercicios_intermediarios_logica/ordenar_vetor_crescente.py
+++ exercicios_intermediarios_logica/ordenar_vetor_crescente.py
@@ -1,5 +1,18 @@
 num = int(input("Digite quantos números terá o vetor: "))
 
+vetor = []
+
 for i in range(num):
     n = float(input(f"Digite o {i + 1}º número: "))
-print(vetor)
+    vetor.append(n)
+
+print(f"Os números que vc digitou são: {vetor}")
+
+for i in range(num - 1):
+    for i in range(num - 1):
+        if (vetor[i] > vetor[i + 1]):
+            aux = vetor[i]
+            vetor[i] = vetor[i + 1]
+            vetor[i + 1] = aux
+    
+print(f"\nOs números em ordem crescente ficam: {vetor}")
